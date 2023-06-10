99b9677d26bacf753c48a9f6f8bf05a260143a6f
somasekimoto <0421.soma@gmail.com>
2023-06-11 00:40:01 +0900

    add: mutation

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,9 @@ import { graphqlServer } from '@hono/graphql-server'
 import { serve } from '@hono/node-server'
 import schema from './schema';
 
-import { PrismaClient } from '@prisma/client';
- 
-
 const app = new Hono()
 
-const prisma = new PrismaClient()
-
-app.get('/create', async (ctx) => {
-    await prisma.user.create({
-        data: {
-          name: "hoge",
-          email: new Date().getTime() + "@example.com",
-        },
-      })
-})
-
-
-app.use(
-  '/graphql',
+app.use('/graphql',
   graphqlServer({
     schema,
   })
--- src/schema.ts
+++ src/schema.ts
@@ -1,21 +1,12 @@
 import { makeSchema } from 'nexus'
-import { objectType, queryType, mutationType } from 'nexus';
+import { objectType, queryType, mutationType, nonNull, stringArg, intArg, idArg } from 'nexus';
 import { PrismaClient } from '@prisma/client'
+// import { createUser } from './types/mutation/user';
 
 const prisma = new PrismaClient()
 
 const schema = makeSchema({
   types: [
-    queryType({
-        definition(t) {
-          t.nonNull.list.nonNull.field('users', {
-            type: 'User',
-            resolve(_, __, ctx) {
-              return prisma.user.findMany()
-            },
-          })
-        },
-      }),
     objectType({
       name: "User",
       definition(t) {
@@ -36,8 +27,53 @@ const schema = makeSchema({
         definition(t) {
             t.nonNull.int('id')
         },
-      })
+    }),
+    queryType({
+        definition(t) {
+          t.nonNull.list.nonNull.field('users', {
+            type: 'User',
+            resolve(_, __, ctx) {
+              return prisma.user.findMany()
+            },
+          })
+        },
+    }),
+    mutationType({
+        definition(t) {
+            t.nonNull.field('createUser', {
+                type: 'User',
+                args: {
+                    name: nonNull(stringArg()),
+                    email: nonNull(stringArg()),
+                },
+                resolve(_parent, {name, email}, ctx) {
+                    return prisma.user.create({ data: { name, email }})
+                },
+            })
+            t.nonNull.field('deleteUser', {
+                type: 'User',
+                args: {
+                    id: nonNull(intArg()),
+                },
+                resolve(_parent, {id}, ctx) {
+                    return prisma.user.delete({ where: { id }})
+                },
+            })
+            t.nonNull.field('updateUser', {
+                type: 'User',
+                args: {
+                    id: nonNull(intArg()),
+                    name: nonNull(stringArg()),
+                    email: nonNull(stringArg()),
+                },
+                resolve(_parent, {id, name, email}, ctx) {
+                    return prisma.user.update({ where: { id }, data: { name, email }})
+                },
+            })
+        },
+    }),
   ],
+
 });
 
 export default schema;
